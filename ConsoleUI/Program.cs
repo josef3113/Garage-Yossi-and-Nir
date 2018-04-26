65405411bb8e8d5f89eded71ba2bb40930dcf574
josef3113 <33261454+josef3113@users.noreply.github.com>
2018-04-26 21:14:00 +0300

    Add Main

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Testing();
+
+        }
+
+        public static void Testing()
+        {
+            Console.WriteLine("new Hey");
+            Console.ReadLine();
+        }
+
+    }
+}
