4c6e586be61a1eb268703c408c623fc3721ca62c
josef3113 <33261454+josef3113@users.noreply.github.com>
2018-04-27 15:16:43 +0300

    add class MotorcycleDetails and class ElectricVehicles  not implemet methods

---
+++ GarageLogic/ElectricVehicles.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageLogic
+{
+    public class ElectricVehicles :Vehicle
+    {
+        private float m_BatteryHours;
+        private float m_MaxHoursBattery;
+
+        public float BatteryHours
+        {
+            get
+            {
+                return m_BatteryHours;
+            }
+
+            set
+            {
+                m_BatteryHours = value;
+            }
+        }
+
+        public float MaxHoursBattery
+        {
+            get
+            {
+                return m_MaxHoursBattery;
+            }
+
+            set
+            {
+                m_MaxHoursBattery = value;
+            }
+        }
+
+        public void ChargingBattery()
+        {
+            // book to do with exption
+        }
+
+        // book i dont know what is this 
+        public override float PersentOfEnergy => throw new NotImplementedException();
+    }
+}
---
+++ GarageLogic/MotorcycleDetails.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageLogic
+{
+    internal class MotorcycleDetails
+    {
+        private int m_EngineCapacity;
+        private eTypeOfLicense m_TypeOfLicense = eTypeOfLicense.A; // difult is A
+
+        public int EngineCapacity
+        {
+            get
+            {
+                return m_EngineCapacity;
+            }
+
+            set
+            {
+                m_EngineCapacity = value;
+            }
+        }
+
+        public eTypeOfLicense TypeOfLicense
+        {
+            get
+            {
+                return m_TypeOfLicense;
+            }
+
+            set
+            {
+                m_TypeOfLicense = value;
+            }
+        }
+    }
+    // book i not sure about all use in enum check this 
+    public enum eTypeOfLicense : byte
+    {
+        A = 1,
+        A1,
+        B1,
+        B2
+    }
+}
