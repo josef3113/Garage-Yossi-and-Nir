3bd91543ef64b855ca63cbfe13f30350a5609769
Nir <Nir@Hey>
2018-04-29 16:36:31 +0300

    Add initWheel to Vehicle.

--- GarageLogic/PetrolCar.cs
+++ GarageLogic/PetrolCar.cs
@@ -9,14 +9,29 @@ namespace GarageLogic
     public class PetrolCar : PetrolVehicle
     {
         CarDetails CarDetails;
+        const eFuelType k_FuelType = eFuelType.Octan98;
 
-        public PetrolCar(string i_ModelName,string i_LicenseNumber) 
+        public PetrolCar(string i_ModelName, string i_LicenseNumber)
             : base(i_ModelName, i_LicenseNumber)
         {
             // book fill the constractor with all fields that needed.
-            CarDetails = new CarDetails(4,"White");
+            m_MaxFuelTank = 45f;
+            m_CurrentFuelTank = 0;
+            m_PercentOfEnergy = 0;
+            CarDetails = new CarDetails(4, "White");
+            m_ListOfWheels = new List<Wheel>(4);
+
+            foreach(Wheel wheel in m_ListOfWheels)
+            {
+                m_ListOfWheels.Add(new Wheel("Unknown", 0, 32));
+            }
+            
+            m_ListOfWheels.Add(new Wheel("Unknown", 0, 32));
+            m_ListOfWheels.Add(new Wheel("Unknown", 0, 32));
+            m_ListOfWheels.Add(new Wheel("Unknown", 0, 32));
+            m_ListOfWheels.Add(new Wheel("Unknown", 0, 32));
         }
-       
+
         public override void Refuel(string i_FuelType, float i_FuelAmout)
         {
             throw new NotImplementedException();
---
+++ GarageLogic/PetrolTrunk.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageLogic
+{
+    class PetrolTrunk : PetrolVehicle
+    {
+        TruckDetails m_TruckDetails;
+
+
+        public PetrolTrunk(string i_Model, string i_LicenseNumber) : base(i_Model, i_LicenseNumber)
+        {
+        }
+
+        public override void Refuel(string i_FuelType, float i_FuelAmout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
